f6d74cc820fb84fe4e06bf40be81c8c07d9ade38
Tom Ritter <tom@ritter.vg>
2017-02-17 13:40:06 -0500

    Lots of fixes and improvements

--- submit-cert.py
+++ submit-cert.py
@@ -1,42 +1,53 @@
 #!/usr/bin/env python
 
+import sys
 import json
 import argparse
 import requests
 
+try:
+	from requests.packages.urllib3.exceptions import InsecureRequestWarning
+	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+except:
+	pass
 
 LOGS = {
-	'Google \'Pilot\' log' : 'https://ct.googleapis.com/pilot',
-	'Google \'Aviator\' log' : 'https://ct.googleapis.com/aviator',
-	'Google \'Rocketeer\' log' : 'https://ct.googleapis.com/rocketeer',
-	'Google\'Icarus\' Log' : 'https://ct.googleapis.com/icarus',
-	'Google \'Skydiver\' Log' : 'https://ct.googleapis.com/skydiver',
-#	'Certly Log Server' : 'https://log.certly.io',
-	'Symantec Log Server' : 'https://ct.ws.symantec.com',
-	'DigiCert Log Server' : 'https://ct1.digicert-ct.com/log',
+	'pilot' : 'https://ct.googleapis.com/pilot',
+	'aviator' : 'https://ct.googleapis.com/aviator',
+	'rocketeer' : 'https://ct.googleapis.com/rocketeer',
+	'icarus' : 'https://ct.googleapis.com/icarus',
+	'skydiver' : 'https://ct.googleapis.com/skydiver',
+#	'certly Log Server' : 'https://log.certly.io',
+	'symantec' : 'https://ct.ws.symantec.com',
+	'digicert' : 'https://ct1.digicert-ct.com/log',
 	#'Google \'Submariner\' log' : 'https://ct.googleapis.com/submariner',
 #	'Izenpe Log Server' :'https://ct.izenpe.com',
-	'Venafi CT Log Server' : 'https://ctlog.api.venafi.com',
-	'Symantec VEGA Log Server' : 'https://vega.ws.symantec.com',
-#	'CNNIC CT Log Server' : 'https://ctserver.cnnic.cn',
-#	'StartSSL' : 'http://ct.startssl.com',
+	'venafi' : 'https://ctlog.api.venafi.com',
+	'vega' : 'https://vega.ws.symantec.com',
+	'cnnic' : 'https://ctserver.cnnic.cn',
+	'startssl' : 'https://ct.startssl.com',
 	#'GDCA CT Log Server' : 'https://ct.gdca.com.cn',
-	#'WoSign CT Log Server' : 'https://ct.wosign.com',
+	'wosign' : 'https://ct.wosign.com',
 	#'Akamai Log' : 'https://ct.akamai.com',
+	'certificatetransparency.cn' : 'https://www.certificatetransparency.cn/ct',
+	'venafigen2' : 'http://ctlog-gen2.api.venafi.com',
 }
 
 parser = argparse.ArgumentParser(description='Submit a certificate to logs')
 parser.add_argument("--cert", type=argparse.FileType('r'), action="append", required=True, 
 	help="Certificate chain. Specify multiple times, start with the leaf, continuing to the root.")
-parser.add_argument("--log", action='append', 
-	choices=['aviator', 'pilot', 'rocketeer', 'digicert', 'izenpen', 'certly', 'venafi', 'skydiver', 'icarus'], 
+parser.add_argument("--log", action='append', type=str,
 	help="logs to submit the cert to. Specify multiple times for explicit choice, or leave blank for all.")
 
 args = parser.parse_args()
 
 certdata = []
+num_certs = 0
 for c in args.cert:
 	lines = ''.join(c.readlines())
+	if lines.count('-----BEGIN CERTIFICATE-----') > 1:
+		print "Error: Specify one certificate per file, with multiple --cert arguments, in the order of leaf, intermediate, root"
+		sys.exit(-1)
 	lines = lines.replace("-----BEGIN CERTIFICATE-----", "")
 	lines = lines.replace("-----END CERTIFICATE-----", "")
 	lines = lines.replace("\r", "")
@@ -49,7 +60,7 @@ data =  '{"chain" : ["' + '", "'.join(certdata) + '"]}'
 for l in LOGS:
 	if not args.log or [x for x in args.log if x in l.lower()]:
 		try:
-			r = requests.post(LOGS[l] + "/ct/v1/add-chain", data=data, verify=requests.certs.where(), timeout=2)
+			r = requests.post(LOGS[l] + "/ct/v1/add-chain", data=data, verify=False, timeout=2)
 			if r.status_code != 200:
 				print("Error {0} while submitting to {1}".format(r.status_code, l))
 				print(r.text)
@@ -58,9 +69,7 @@ for l in LOGS:
 				print(l)
 				print("\tTimestamp", r['timestamp'])
 				print("\tSignature", r['signature'])
-				print("\tID", r['id'])
-				print("\tExtensions", r['extensions'])
-				print("\tSCT Version", r['sct_version'])
+				print("\tCommand: ./write-sct.py --stdout --time " + str(r['timestamp']) + " --sig " + str(r['signature']) + " --log " + l)
 		except Exception as e:
 			print("Error communicating with", l)
 			print(e)
--- write-sct.py
+++ write-sct.py
@@ -13,13 +13,20 @@ LOGS = {
 	'certly' : base64.b64decode("zbUXm3/BwEb+6jETaj+PAC5hgvr4iW/syLL1tatgSQA="),
 	'venafi' : base64.b64decode("rDua7X+pZ0dXFZ5tfVdWcvnZgQCUHpve/+yhMTt1eC0="),
         'digicert' : base64.b64decode("VhQGmi/XwuzT9eG9RLI+x0Z2ubyZEVzA75SYVdaJ0N0="),
-        'skydiver' : base64.b64decode("u9nfvB+KcbWTlCOXqpJ7RzhXlQqrUugakJZkNo4e0YU=")
+        'skydiver' : base64.b64decode("u9nfvB+KcbWTlCOXqpJ7RzhXlQqrUugakJZkNo4e0YU="),
+        'icarus' : base64.b64decode("KTxRllTIOWW6qlD8WAfUt2+/WHopctykwwz05UVH9Hg="),
+        'vega' : base64.b64decode("vHjh38X2PGhGSTNNoQ+hXwl5aSAJwIG08/aRfz7ZuKU="),
+        'wosign' : base64.b64decode("QbLcLonmPOSvG6e7Kb9oxt7m+fHMBH4w3/rjs7olkmM="),
+        'cnnic' : base64.b64decode("pXesnO11SN2PAltnokEInfhuD0duwgPC7L7bGF8oJjg="),
+        'startssl' : base64.b64decode("NLtq1sPfnAPuqKSZ/3iRSGydXlysktAfe/0bzhnbSO8="),
+        'certificatetransparency.cn' : base64.b64decode("4BJ2KekEllZOPQFHmESYqkj4rbFmAOt5AqHvmQmQYnM="),
+        'venafigen2' : base64.b64decode("AwGd8/2FppqOvR+sxtqbpz5Gl3T+d/V5/FoIuDKMHWs="),
 }
 
 parser = argparse.ArgumentParser(description='Write a SCT')
 parser.add_argument("--out", type=argparse.FileType('w'), help="file to write out to")
 parser.add_argument("--stdout", action="store_true", help="write to stdout, to be used in echo \"...\" | base64 -d > file")
-parser.add_argument("--log", choices=['aviator', 'pilot', 'rocketeer', 'certly', 'digicert', 'izenpen', 'venafi', 'skydiver', 'digicert'], required=True)
+parser.add_argument("--log", type=str, required=True)
 parser.add_argument("--time", "--timestamp", type=int, required=True, help="Timestamp from the JSON response.")
 parser.add_argument("--sig", type=str, required=True, help="Signature value from the JSON response, base64 encoded")
 
@@ -27,6 +34,9 @@ args = parser.parse_args()
 if not args.out and not args.stdout:
     parser.print_usage()
     print "\nError: Either --out or --stdout must be specified"
+    exit(-1)
+if args.log not in LOGS:
+    print "Error: log not known. Choose one of", str(LOGS.keys())
     exit(-1)
 
 sct = ""
